d8073b80d47aafb6ecdf60fd74bfaa7cd9acb709
The Goose <trevordammon@gmail.com>
2019-03-05 17:51:39 -0600

    working on recently rated page

--- src/components/RecentlyRatedPage/RecentlyRatedPage.js
+++ src/components/RecentlyRatedPage/RecentlyRatedPage.js
@@ -1,8 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {withStyles} from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 
 const styles = theme => ({
+    card: {
+        border: 'black',
+    },
+    beerName: {
+        display: 'flex',
+        justifyContent: 'center',
+        margin: 0,
+    },
+    inlinetypography: {
+        // display: 'inline',
+        float: 'left',
+        paddingLeft: 10,
+    },
+    img : {
+        float: 'right',
+        width: 100,
+        height: 100,
+        
+    }
 })
 
 class RecentlyRatedPage extends Component {
@@ -16,11 +39,41 @@ class RecentlyRatedPage extends Component {
 
 
 render() {
-
+    const {classes} = this.props
     return(
     <div>
-        <p>Hello</p>
-        {JSON.stringify(this.props.ratings)}
+        {/* {JSON.stringify(this.props.ratings)} */}
+        {this.props.ratings.map(rating => {
+            return(
+                <Card className={classes.card}>
+                    <div>
+                        <CardContent>
+                            <div className={classes.beerName}>
+                            <Typography variant="headline">{rating.name}</Typography>
+                            </div>
+                            <div className={classes.inlinetypography}>
+                            <Typography>Aroma:  </Typography>
+                            <Typography>Color:</Typography>
+                            <Typography>Flavor:</Typography>
+                            <Typography>Finish:</Typography>
+                            </div>
+                            <div className={classes.inlinetypography}>
+                            <Typography>{rating.aroma}</Typography>
+                            <Typography>{rating.color}</Typography>
+                            <Typography>{rating.flavor}</Typography>
+                            <Typography>{rating.finish}</Typography>
+                            </div>
+                        </CardContent>
+                    </div>
+                    <div >
+                    <CardMedia 
+                    className={classes.img}
+                    image="images/Frog.png"
+                    />
+                    </div>
+                </Card>
+            )
+        })}
     </div>
     )
 }
